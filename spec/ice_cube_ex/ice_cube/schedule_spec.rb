eab2e1abdb27ccdd822a0c0b849cf7f63b8bbdfc
junhanamaki <jun.hanamaki@gmail.com>
2014-07-27 22:40:55 +0100

    amazing...

--- spec/ice_cube_ex/ice_cube/schedule_spec.rb
+++ spec/ice_cube_ex/ice_cube/schedule_spec.rb
@@ -25,6 +25,8 @@ describe IceCube::Schedule do
           let(:second_occurrence) { schedule.next_occurrence first_occurrence }
           let(:third_occurrence)  { schedule.next_occurrence second_occurrence }
           let(:fourth_occurrence) { schedule.next_occurrence third_occurrence }
+          let(:fifth_occurrence)  { schedule.next_occurrence fourth_occurrence }
+          let(:sixth_occurrence)  { schedule.next_occurrence fifth_occurrence }
 
           it 'returns 4-2-2012 at 12am as first occurrence' do
             expect(first_occurrence).to eq(start_time)
@@ -40,6 +42,79 @@ describe IceCube::Schedule do
 
           it 'returns 9-2-2012 at 12am as fourth occurrence' do
             expect(fourth_occurrence).to eq(Time.new(2012, 2, 9, 12))
+          end
+
+          it 'returns 12-2-2012 at 12am as fifth occurrence', test: true do
+            expect(fifth_occurrence).to eq(Time.new(2012, 2, 12, 12))
+          end
+
+          it 'returns 13-2-2012 at 12am as sixth occurrence' do
+            expect(sixth_occurrence).to eq(Time.new(2012, 2, 13, 12))
+          end
+        end
+      end
+
+      context 'given rule of type DailyCycleRule initialized to ' \
+              'every 4 days (cycle), ' \
+              'repeat 2 days (repeat), ' \
+              'which repeats 3 times (count)' do
+        let(:repeat) { 2 }
+        let(:cycle)  { 4 }
+        let(:count)  { 3 }
+        let(:rule)   { IceCube::DailyCycleRule.new(cycle, repeat).count(3) }
+
+        context 'calculating from 3-2-2012' do
+          let(:from) { Time.new(2012, 2, 3) }
+          let(:first_occurrence)  { schedule.next_occurrence from }
+          let(:second_occurrence) { schedule.next_occurrence first_occurrence }
+          let(:third_occurrence)  { schedule.next_occurrence second_occurrence }
+          let(:fourth_occurrence) { schedule.next_occurrence third_occurrence }
+
+          it 'returns 4-2-2012 at 12am as first occurrence' do
+            expect(first_occurrence).to eq(start_time)
+          end
+
+          it 'returns 5-2-2012 at 12am as second occurrence' do
+            expect(second_occurrence).to eq(Time.new(2012, 2, 5, 12))
+          end
+
+          it 'returns 8-2-2012 at 12am as third occurrence', test: true do
+            expect(third_occurrence).to eq(Time.new(2012, 2, 8, 12))
+          end
+
+          it 'returns nil as fourth occurrence' do
+            expect(fourth_occurrence).to eq(nil)
+          end
+        end
+      end
+
+      context 'given rule of type DailyCycleRule initialized to ' \
+              'every 4 days (cycle), ' \
+              'repeat 2 days (repeat), ' \
+              'which repeats until 6-2-2012' do
+        let(:repeat)       { 2 }
+        let(:cycle)        { 4 }
+        let(:repeat_until) { Time.new(2012, 2, 6) }
+        let(:rule) do
+          IceCube::DailyCycleRule.new(cycle, repeat).until(repeat_until)
+        end
+
+        context 'calculating from 3-2-2012' do
+          let(:from) { Time.new(2012, 2, 3) }
+          let(:first_occurrence)  { schedule.next_occurrence from }
+          let(:second_occurrence) { schedule.next_occurrence first_occurrence }
+          let(:third_occurrence)  { schedule.next_occurrence second_occurrence }
+
+          it 'returns 4-2-2012 at 12am as first occurrence' do
+            expect(first_occurrence).to eq(start_time)
+          end
+
+          it 'returns 5-2-2012 at 12am as second occurrence' do
+            expect(second_occurrence).to eq(Time.new(2012, 2, 5, 12))
+          end
+
+          it 'returns nil as third occurrence', test: true do
+            expect(third_occurrence).to eq(nil)
           end
         end
       end
